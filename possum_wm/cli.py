5328123c17b0b39b750935c6463f94d6b716236a
Ellen Marie Dash <me@duckie.co>
2019-12-08 07:23:02 -0500

    wheeeeeeee

--- possum_wm/cli.py
+++ possum_wm/cli.py
@@ -1,8 +1,29 @@
+import sys
+
 from . import __author__, __version__
 
+from .wm import PossumWM
+
 def version():
-    print("possum v{} by {}.".format(__version__, __author__))
+    print("PossumWM v{} by {}.".format(__version__, __author__))
 
 def main():
-    print("TODO")
-    exit(1)
+    if "--version" in sys.argv:
+        version()
+        exit()
+
+    wm = PossumWM()
+    try:
+        print("*** PossumWM Starting.")
+        print("* Version {}.".format(__version__))
+        wm.start()
+        for line in wm.info():
+            print("* {}.".format(line))
+        wm.loop()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print()
+        print("*** PossumWM Stopping.")
+        wm.stop()
+
--- possum_wm/wm.py
+++ possum_wm/wm.py
@@ -1,1 +1,31 @@
-# TODO: Actually implement things.
+import Xlib
+import Xlib.display
+
+import time
+
+# QUESTIONABLE ASSUMPTIONS:
+# - only the first screen is used/cared about.
+class PossumWM:
+    def start(self):
+        # Display name is inferred from $DISPLAY environment variable.
+        self.display = Xlib.display.Display()
+        self.screen = self.display.get_default_screen()
+        self.root = self.display.screen().root
+
+    def info(self):
+        g = self.root.get_geometry()
+        return [
+            "root window dimensions = {}x{}".format(g.width, g.height),
+        ]
+
+    def stop(self):
+        self.display.close()
+        self.display = None
+
+    def handle(self, event):
+        print("[PossumWM.handle] Got event!")
+        print("  ", event)
+
+    def loop(self):
+        while True:
+            self.handle(self.display.next_event())
